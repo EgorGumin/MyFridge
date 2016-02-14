0426edf1acb5cccb782b6d5a85bc6be99e1872f6
Gumin Egor <gumin.egor@gmail.com>
2016-02-14 21:58:52 +0300

    Удалены неиспользуемые лэйауты

--- app/src/main/java/com/lymno/myfridge/activity/Drawer.java
+++ app/src/main/java/com/lymno/myfridge/activity/Drawer.java
@@ -1,9 +1,7 @@
 package com.lymno.myfridge.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.lymno.myfridge.R;
 import com.lymno.myfridge.fragment.DrawerProducts;
@@ -28,14 +25,6 @@ public class Drawer extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
